5414f93f8b75f94a826ac6bcb8a38dcc465049a6
royik <ryoheiikezawa@protonmail.com>
2022-10-06 01:15:52 +0900

    update

--- blocks.def.h
+++ blocks.def.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"UD : ", "~/.local/src/suckless/dwmblocks/scripts/update", 1000, 0},
+	{"UD:", "~/.local/src/suckless/dwmblocks/scripts/update", 1000, 0},
 
-	{"CPU_Temp : ", "~/.local/src/suckless/dwmblocks/scripts/cpu_temp", 5, 0},
+	{"CPU:", "~/.local/src/suckless/dwmblocks/scripts/cpu_usage", 5, 0},
+    
+	{"CPU_Temp:", "~/.local/src/suckless/dwmblocks/scripts/cpu_temp", 5, 0},
 
-	{"CPU : ", "~/.local/src/suckless/dwmblocks/scripts/cpu_usage", 5, 0},
+	{"Mem:", "~/.local/src/suckless/dwmblocks/scripts/memory", 5, 0},
 
-	{"Mem : ", "~/.local/src/suckless/dwmblocks/scripts/memory", 5, 0},
+	{"Disk:", "~/.local/src/suckless/dwmblocks/scripts/disk", 60, 0},
 
-	{"Disk : ", "~/.local/src/suckless/dwmblocks/scripts/disk", 60, 0},
-
-	{"Bat : ", "~/.local/src/suckless/dwmblocks/scripts/battery", 5, 0},
+	{"Bat:", "~/.local/src/suckless/dwmblocks/scripts/battery", 5, 0},
     
 	{"", "~/.local/src/suckless/dwmblocks/scripts/timedate", 5, 0},
 };
