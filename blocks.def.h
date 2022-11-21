acfe8407778ca2835fffb087da7e4187afe2d250
royik <ryoheiikezawa@protonmail.com>
2022-11-21 23:35:38 +0900

    update

--- blocks.def.h
+++ blocks.def.h
@@ -1,25 +1,25 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"   ", "~/.local/src/suckless/dwmblocks/scripts/update", 1000, 0},
+    {"  ", "~/.local/src/suckless/dwmblocks/scripts/update", 1000, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/cpu_usage", 2, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/cpu_usage", 2, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/cpu_temp", 2, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/cpu_temp", 2, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/memory", 2, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/memory", 2, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/disk", 60, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/disk", 60, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/battery", 6, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/battery", 6, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/uptime", 30, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/uptime", 30, 0},
 
-    {"   ", "uname -r", 0, 0},
+    {"  ", "uname -r", 0, 0},
 
-	{"   ", "~/.local/src/suckless/dwmblocks/scripts/timedate", 10, 0},
+	{"  ", "~/.local/src/suckless/dwmblocks/scripts/timedate", 10, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
